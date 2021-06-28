6407695492471b45f9de978af7cc7f131eaf6f8d
sy-app <seijuslp4@icloud.com>
2021-06-29 01:12:41 +0900

    fetch_stored_tweets関数の基本処理を実装

--- hello_world_function/hello_world/fetch_stored_tweets.py
+++ hello_world_function/hello_world/fetch_stored_tweets.py
@@ -6,7 +6,10 @@ BUCKET_KEY = 'stored_tweets.json'
 
 
 def fetch_stored_tweets():
-    s3 = boto3.client('s3')
-    res = s3.get_object(Bucket=BUCKET_NAME, Key=BUCKET_KEY)
-    stored_tweets = json.loads(res['Body'].read().decode('utf-8'))
-    return stored_tweets
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET_NAME)
+    obj = bucket.Object(BUCKET_KEY)
+    response = obj.get()
+    body = response['Body'].read()
+    tweets = json.loads(body.decode('utf-8'))
+    return tweets
