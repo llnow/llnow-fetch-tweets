2e011acb8b9bd54d0d7266b16d7dd79b2c9eb477
sy-app <seijuslp4@icloud.com>
2021-06-28 23:09:52 +0900

    構成を変更

---
+++ hello_world_function/hello_world/main.py
@@ -0,0 +1,20 @@
+from fetch_new_tweet import *
+
+from requests_oauthlib import OAuth1Session
+import os
+
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+THRESHOLD = 1000 # WordCloudを生成するために必要なツイート数
+
+twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+
+def main(event, context):
+    stored_tweets = fetch_stored_tweet()
+    new_tweets = fetch_new_tweet()
+    tweets = merge_tweet(stored_tweets, new_tweets)
+    put_tweet(tweets)
