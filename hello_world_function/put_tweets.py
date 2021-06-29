b1e0fc6208588eab0b88714451fa1a180e9de9f4
sy-app <seijuslp4@icloud.com>
2021-06-30 02:56:10 +0900

    read-onlyのエラーを回避するためにlambda内のパスを修正

--- hello_world_function/put_tweets.py
+++ hello_world_function/put_tweets.py
@@ -7,7 +7,7 @@ BUCKET_NAME = 'll-now-material'
 def put_tweets(tweets, THRESHOLD):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(BUCKET_NAME)
-    tweets_path = 'tmp/tweets.json'
+    tweets_path = '/tmp/tweets.json'
     with open(tweets_path, 'w') as f:
         json.dump(tweets, f, indent=4)
     # ready_tweetsとしてs3に格納
