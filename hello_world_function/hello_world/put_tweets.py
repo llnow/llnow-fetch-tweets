b589bf03346fbdf0ba311e488e67860d71c33a29
sy-app <seijuslp4@icloud.com>
2021-06-29 23:54:43 +0900

    基本処理を実装

--- hello_world_function/hello_world/put_tweets.py
+++ hello_world_function/hello_world/put_tweets.py
@@ -1,7 +1,20 @@
+import boto3
+import json
+
+BUCKET_NAME = 'll-now-material'
+
+
 def put_tweets(tweets, THRESHOLD):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET_NAME)
+    tweets_path = 'tmp/tweets.json'
+    with open(tweets_path, 'w') as f:
+        json.dump(tweets, f, indent=4)
     # ready_tweetsとしてs3に格納
     if len(tweets) >= THRESHOLD:
-        pass
+        # アップロード(上書き)
+        bucket.upload_file(tweets_path, 'tmp/ready_tweets.json')
     # stored_tweetsとしてs3に格納
     else:
-        pass
+        # アップロード(上書き)
+        bucket.upload_file(tweets_path, 'tmp/stored_tweets.json')
