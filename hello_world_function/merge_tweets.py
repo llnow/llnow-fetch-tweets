9bda9eb713371cdc431719d02b46666cad477c59
sy-app <seijuslp4@icloud.com>
2021-06-30 23:46:01 +0900

    extendに関するミスを修正

--- hello_world_function/merge_tweets.py
+++ hello_world_function/merge_tweets.py
@@ -2,5 +2,5 @@ def merge_tweets(stored_tweets, new_tweets):
     if stored_tweets is None:
         return new_tweets
     else:
-        tweets = new_tweets.extend(stored_tweets)
-        return tweets
+        new_tweets.extend(stored_tweets)
+        return new_tweets
