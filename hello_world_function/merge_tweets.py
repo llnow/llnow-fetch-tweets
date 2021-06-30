e54cd02c670823a92621376ba49a4764b79eba9e
sy-app <seijuslp4@icloud.com>
2021-07-01 00:58:20 +0900

    add comment

--- hello_world_function/merge_tweets.py
+++ hello_world_function/merge_tweets.py
@@ -2,5 +2,5 @@ def merge_tweets(stored_tweets, new_tweets):
     if stored_tweets is None:
         return new_tweets
     else:
-        new_tweets.extend(stored_tweets)
+        new_tweets.extend(stored_tweets)  # new_tweetsにstored_tweetsを連結
         return new_tweets
