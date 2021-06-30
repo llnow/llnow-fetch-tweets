b4c5f6c2d740539ad396989af101df9941f709f2
sy-app <seijuslp4@icloud.com>
2021-06-30 23:28:27 +0900

    取得ツイート数が十分な場合，貯蔵していたツイートのファイルを削除

--- hello_world_function/put_tweets.py
+++ hello_world_function/put_tweets.py
@@ -6,15 +6,18 @@ BUCKET_NAME = 'll-now-material'
 
 def put_tweets(tweets, THRESHOLD):
     s3 = boto3.resource('s3')
+    s3_client = boto3.client('s3')  # s3のファイル削除用
     bucket = s3.Bucket(BUCKET_NAME)
     tweets_path = '/tmp/tweets.json'
     with open(tweets_path, 'w') as f:
         json.dump(tweets, f, indent=4, ensure_ascii=False)
-    # ready_tweetsとしてs3に格納
+    # 取得ツイート数が十分な場合
     if len(tweets) >= THRESHOLD:
-        # アップロード(上書き)
+        # 不要なstored_tweetsを削除
+        s3_client.delete_object(Bucket=BUCKET_NAME, Key='tmp/stored_tweets.json')
+        # ready_tweetsとしてs3に格納(次の処理のトリガー)
         bucket.upload_file(tweets_path, 'tmp/ready_tweets.json')
-    # stored_tweetsとしてs3に格納
+    # 取得ツイート数が不十分な場合
     else:
-        # アップロード(上書き)
+        # stored_tweetsとしてs3に格納
         bucket.upload_file(tweets_path, 'tmp/stored_tweets.json')
