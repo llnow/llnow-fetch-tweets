b954dcf4e89d13b1689fa992484177f1df11f5fe
sy-app <seijuslp4@icloud.com>
2021-07-05 23:10:21 +0900

    Use set_twitter()

--- hello_world_function/main.py
+++ hello_world_function/main.py
@@ -1,21 +1,16 @@
+from set_twitter import *
 from fetch_stored_tweets import *
 from fetch_new_tweets import *
 from merge_tweets import *
 from put_tweets import *
-from requests_oauthlib import OAuth1Session
 import os
 
-
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
-N_REQUIRED_TWEETS = int(os.environ['N_REQUIRED_TWEETS'])  # WordCloudを生成するために必要なツイート数
-
-twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# WordCloudを生成するために必要なツイート数
+N_REQUIRED_TWEETS = int(os.environ['N_REQUIRED_TWEETS'])
 
 
 def main(event, context):
+    twitter = set_twitter()
     stored_tweets = fetch_stored_tweets()
     new_tweets = fetch_new_tweets(twitter)
     tweets = merge_tweets(stored_tweets, new_tweets)
