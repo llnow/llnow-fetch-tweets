a034a862e42b99d4c60d3eacb46865e9fa153e3a
sy-app <seijuslp4@icloud.com>
2021-07-25 22:43:40 +0900

    Add comment

--- app/get_query.py
+++ app/get_query.py
@@ -4,14 +4,15 @@ import boto3
 def get_query():
     table = boto3.resource('dynamodb').Table('ll_now')
 
+    # 検索キーワードを取得
     primary_key = {'primary': 'search_keyword'}
     res = table.get_item(Key=primary_key)
     keywords = res['Item']['keyword']
-
+    # 検索フィルターを取得
     primary_key = {'primary': 'search_filter'}
     res = table.get_item(Key=primary_key)
     filters = res['Item']['filter']
-
+    # 除外したいユーザを取得
     primary_key = {'primary': 'invalid_user_list'}
     res = table.get_item(Key=primary_key)
     users = res['Item']['user']
