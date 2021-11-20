f6e237862da5ac5b0f3da8d419a4b0a9d4111a0d
sy-app <seijuslp4@icloud.com>
2021-11-21 02:45:11 +0900

    modeに関わらずdev用バケットにツイートを置き、prodの場合はprod用バケットにもツイートを置く

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from put_tweets import *
 import os
 
 # WordCloudを生成するために必要なツイート数
-N_REQUIRED_TWEETS = int(os.environ['N_REQUIRED_TWEETS'])
+n_required_tweets = int(os.environ['N_REQUIRED_TWEETS'])
 # ツイート取得リクエストの間隔(秒)
 sleep_sec = int(os.environ['SLEEP_SEC'])
 # max_api_request_force
@@ -14,8 +14,9 @@ max_api_request_force = int(os.environ['MAX_API_REQUEST_FORCE'])
 
 
 def main(event, context):
+    mode = context.invoked_function_arn.split(':')[-1]
     twitter = set_twitter()
     stored_tweets = fetch_stored_tweets()
     new_tweets = fetch_new_tweets(twitter, sleep_sec, max_api_request_force)
     tweets = merge_tweets(stored_tweets, new_tweets)
-    put_tweets(tweets, N_REQUIRED_TWEETS)
+    put_tweets(tweets, n_required_tweets, mode)
