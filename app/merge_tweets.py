322a1baad5df8473eac3407c866f84335ead8dc2
sy-app <seijuslp4@icloud.com>
2021-12-05 18:34:37 +0900

    一度の取得の間に急激にツイートが増加していれば次の処理を呼び出す

--- app/merge_tweets.py
+++ app/merge_tweets.py
@@ -1,6 +1,22 @@
-def merge_tweets(stored_tweets, new_tweets):
+from get_ssm_params import *
+
+
+def merge_tweets(stored_tweets, new_tweets, n_required_tweets, mode):
+    flag_trigger_next_process = False
+    # 新しく取得したツイートの数の閾値をssmパラメータストアから取得
+    key = 'll-now-threshold-rapid-tweets-increase-{}'.format(mode)
+    params = get_ssm_params(key)
+    threshold_rapid_tweets_increase = int(params[key])
+
+    if len(new_tweets) >= threshold_rapid_tweets_increase:
+        flag_trigger_next_process = True
+
     if stored_tweets is None:
-        return new_tweets
+        tweets = new_tweets
     else:
-        new_tweets.extend(stored_tweets)  # new_tweetsにstored_tweetsを連結
-        return new_tweets
+        tweets = new_tweets + stored_tweets  # new_tweetsとstored_tweetsを連結
+
+    if len(tweets) >= n_required_tweets:
+        flag_trigger_next_process = True
+
+    return tweets, flag_trigger_next_process
